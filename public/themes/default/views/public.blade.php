39f35a48da63c49d8776388f8092e2844108755b
dheamonnica <dheamonnica.acc@gmail.com>
2024-05-24 21:12:25 +0700

    feat: improvement homepage

--- public/themes/default/views/public.blade.php
+++ public/themes/default/views/public.blade.php
@@ -625,6 +625,88 @@
     <!--//End Our Team -->
     <!--==================== Testimonials ====================-->
     <!--==================== Footer ====================-->
+    <footer>
+        <div class="container container-custom">
+            <div class="row">
+                <div class="col-sm-12 col-md-12 col-lg-4">
+                    <div class="foot-contact-block">
+                        <img src="/front/images/RMI_logo_small-removebg-preview.png" class="img-fluid"
+                            alt="#" />
+                        <p>
+                            The best solution for purchasing medical equipment with one-day delivery, supported by an
+                            online tracking system that makes it easier for clients.
+                        </p>
+                        <a href="tel:021-8759842">
+                            <h4><i class="fas fa-phone"></i>021-8759842</h4>
+                        </a>
+                        <a href="mailto:officebogor@rizqullahmediskaindonesia.com">
+                            <h4><i class="far fa-envelope"></i>support@rmi.com</h4>
+                        </a>
+                        <a href="#">
+                            <h4><i class="far fa-clock"></i>07.30 - 17.00</h4>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-2 offset-lg-1">
+                    <div class="foot-link-box">
+                        <h4>Quick Links</h4>
+                        <ul>
+                            <li>
+                                <a href="#about"><i class="fas fa-angle-double-right"></i>About Us</a>
+                            </li>
+                            <li>
+                                <a href="#tracking"><i class="fas fa-angle-double-right"></i>Order Tracking</a>
+                            </li>
+                            <li>
+                                <a href="#service"><i class="fas fa-angle-double-right"></i>Service</a>
+                            </li>
+                            <li>
+                                <a href="#location"><i class="fas fa-angle-double-right"></i>Location Coverage</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-4 col-lg-2">
+                    <div class="foot-link-box">
+                        <h4>Our Product & Services</h4>
+                        <ul>
+                            <li>
+                                <a href="/register"><i class="fas fa-angle-double-right"></i>Register</a>
+                            </li>
+                            <li>
+                                <a href="/login"><i class="fas fa-angle-double-right"></i>Login</a>
+                            </li>
+                            <li>
+                                <a href="/offering"><i class="fas fa-angle-double-right"></i>Offering</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-4 col-lg-2 offset-lg-1">
+                    <div class="foot-link-box footlink-box_btn">
+                        <a href="https://www.google.co.id/maps/place/PT+Rizqullah+Mediska+Indonesia/@-6.468258,106.8216806,17z/data=!3m1!4b1!4m6!3m5!1s0x2e69c1fbffb892a1:0xf8e8845ca31ed0e4!8m2!3d-6.468258!4d106.8242555!16s%2Fg%2F11fy4pq_wl?entry=ttu"
+                            class="btn btn-outline-success">Bogor</a>
+                        <a href="https://www.google.co.id/maps/place/PT+Rizqullah+Mediska+Indonesia+Bekasi/@-6.2821745,107.0091687,17z/data=!3m1!4b1!4m6!3m5!1s0x2e698f6f853f70f1:0xaa792ca4bd34b03e!8m2!3d-6.2821745!4d107.0117436!16s%2Fg%2F11lm27t7gz?entry=ttu"
+                            class="btn btn-outline-success">Bekasi</a>
+                        <a href="https://www.google.co.id/maps/place/PT+Rizqullah+Mediska+Indonesia+Tangerang/@-6.1508431,106.5916991,17z/data=!3m1!4b1!4m6!3m5!1s0x2e69ffe56a3ad283:0x29bba9e8459c18f2!8m2!3d-6.1508431!4d106.594274!16s%2Fg%2F11q8gy5lxv?entry=ttu"
+                            class="btn btn-outline-success">Tangerang</a>
+                        <a href="https://www.google.co.id/maps/place/PT.+Rizqullah+Mediska+Indonesia+Bandung/@-6.9189295,107.6669802,17z/data=!3m1!4b1!4m6!3m5!1s0x2e68dd7b7c60fd1f:0xb86b210e8774f0e7!8m2!3d-6.9189295!4d107.6695551!16s%2Fg%2F11t9tpck0s?entry=ttu"
+                            class="btn btn-outline-success">Bandung</a>
+                        <a href="https://www.google.co.id/maps/place/PT+Rizqullah+Mediska+Indonesia+Jawa+Tengah+Solo/@-7.4225981,108.0550889,7.33z/data=!4m6!3m5!1s0x2e7a15deaa87fc73:0x90a5c2885d3e848c!8m2!3d-7.5366952!4d110.7998541!16s%2Fg%2F11rvcvvd7r?entry=ttu"
+                            class="btn btn-outline-success">Solo</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="copyright">
+                        <p>© RMI 2024 Allright Reserved</p>
+                        <a href="#" id="scroll"><i class="fas fa-angle-double-up"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
     @include('theme::sections.front_footer')
 
     <script>
