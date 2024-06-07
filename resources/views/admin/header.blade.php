6261365203dc44b3f442c803fd1ae23675c0047a
dheamonnica <dheamonnica.acc@gmail.com>
2024-06-07 22:22:20 +0700

    feat: add header logo

--- resources/views/admin/header.blade.php
+++ resources/views/admin/header.blade.php
@@ -2,6 +2,8 @@
 <header class="main-header">
   <!-- Logo -->
   <a href="{{ url('/') }}" class="logo">
+    <img src="{{ get_storage_file_url(optional($system->iconImage)->path) }}" width="30" alt="{{ trans('app.icon') }}"> {{ get_site_title() }}
+
     <!-- mini logo for sidebar mini 50x50 pixels -->
     <span class="logo-mini">{{ Str::limit(get_site_title(), 2, '.') ? Str::limit(get_site_title(), 2, '.') : "RMI" }}</span>
 
